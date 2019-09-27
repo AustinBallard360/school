b05c2fee874ffdef1e09d6a100eec14dcab5bab4
luke.schulz <luke_schulz@gmail.com>
2019-09-26 19:47:53 -0700

    update to heap c++

--- code/C++/data_structures/heap.cpp
+++ code/C++/data_structures/heap.cpp
@@ -4,8 +4,10 @@
 using namespace std;
 
 
-int arr[] ={10,9,8,7,6,5,4,3,2,1,0};
-int arrSize = sizeof(arr) / sizeof(10); // get size of array / /we gonna need to check for tree bounds on parent left and right funcs
+int arrMin2Max[] ={0,1,2,3,4,5,6,7,8,9,10};
+int arrMax2Min[] ={10,9,8,7,6,5,4,3,2,1,0};
+int arrRandom[] ={5,1,8,9,2,10,3,7,4,1,0};
+int arrSize = sizeof(arrMin2Max) / sizeof(10); // get size of array / /we gonna need to check for tree bounds on parent left and right funcs
 
 void heapify( int arr[] ,int i ,int n){ // array, index, sizeofarray
 	int largest = i; // assume current index is the largest?
@@ -21,9 +23,6 @@ void heapify( int arr[] ,int i ,int n){ // array, index, sizeofarray
 	} // if element is larger than its parent, and exists
 	
 	if( largest != i){ // if we found a larger element
-		// int tempIVal = arr[i]; // just used swap()
-		// arr[i] = arr[largest]; // just used swap()
-		// arr[largest] = tempIVal; // just used swap()
 		swap(arr[i] , arr[largest]); // do we need to include <algorithm> to use this functioun? idk
 		heapify(arr , largest, n ); // recurse on our new largest node. // else done
 	}
@@ -33,46 +32,76 @@ void buildHeap(int arr[] ,  int n) { // array, and size of array
 	int startingIndex = ( n / 2) + 1; // should be largest non leaf node ?
 	for(int i=startingIndex ; i >= 0 ; i-- ){ // starting at our last non leaf node, and moving to the front of array.
 		heapify(arr , i , n);
+		//cout << " I Heapified\n";
 	}
 }
-int left(int i){
-	int nodeIndex = 2*i; // if heaped, left child should always be x
-	if(nodeIndex > arrSize){ // check if child is inside of array bounds
+int parent( int* arr, int i){
+	int nodeIndex = (i-1)/2;
+	int node = arr[nodeIndex];
+		return node;
+}
+int left(int* arr, int i){
+	int nodeIndex = (2*i)+1; // if heaped, left child should always be x
+	if(nodeIndex < arrSize){ // check if child is inside of array bounds
 		int node = arr[nodeIndex];
 		return node;
 	}
 	else{
-		cout << "invalid bounds, DNE";
+		cout << "IS LEAF NODE";
 		return -1 ;		
 	}
 }
-int right(int i){
-	int nodeIndex = (2*i)+1;
-	if(nodeIndex > arrSize){ // check if child is inside of array bounds
+int right(int* arr , int i){
+	int nodeIndex = (2*i)+2;
+	if(nodeIndex < arrSize){ // check if child is inside of array bounds
 		int node = arr[nodeIndex];
 		return node;
 	}
 	else{
-		cout << "invalid bounds, DNE";
+		cout << "IS LEAF NODE";
 		return -1;		
 	}
 
 }
 void printHeap(int arr[], int n)  // stole this whole function
 { 
-    cout << "Array representation of Heap is:\n"; 
+   // cout << "Array representation of Heap is:\n"; 
   
     for (int i = 0; i < n; ++i) 
         cout << arr[i] << " "; 
     cout << "\n"; 
 } 
+void printFamily(int* arr, int size){
+	for( int c = 0 ; c < (size/2) ; c++){	// loop thru array (minus leaf nodes hopefully);
+		if(c==0){
+			cout << "ROOT IS "<<arr[c] <<endl;
+		}else{
+			cout << "Index = " << c << " value = " << arr[c] <<endl;
+			cout << "\tParentIndex = " << (c-1)/2 << " Parent Val =  " << parent(arr , c) <<endl;
+			cout << "\t\tleftIndex = " << (c*2)+1 << " Left Val =  " << left(arr , c) <<endl;
+			cout << "\t\trightIndex = " << (c*2)+2 << " Right Val =  " << right(arr , c) <<endl <<endl;
+		}		
+	}
+}
 
 int main(){
+	// this is just driver program to try and display some output for me to make sense of whats going on
 	cout << "Should heapify things?" <<endl;
-	cout << "array is as follows" <<endl;
-	printHeap(arr , arrSize);
-	buildHeap(arr, arrSize);
-	printHeap(arr, arrSize);
+	cout << " Arry min to max HEAPED\n";
+	buildHeap(arrMin2Max, arrSize);
+	printHeap(arrMin2Max, arrSize);
 
-	return 1;
+	cout << "Array max to min HEAPED\n";
+	buildHeap(arrMax2Min, arrSize);
+	printHeap(arrMax2Min, arrSize);
+
+	cout << "Array random HEAPED\n";
+	buildHeap(arrRandom, arrSize);
+	printHeap(arrRandom, arrSize);	
+	// these three heaps dont match up, im not sure whats up.
+	cout << "\n\n Heres the printFamily for heaped min2max array \n";
+	printHeap(arrMin2Max, arrSize);
+	printFamily(arrMin2Max , arrSize);
+	printHeap(arrMin2Max, arrSize);
+	return 0;
 }
